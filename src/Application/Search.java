b01a4689329998a16748235aca238a59cffbec8d
duanyanan <duanyanan96@outlook.com>
2017-05-14 13:44:32 +0800

    加了Detail但是还没查出来

--- src/Application/Search.java
+++ src/Application/Search.java
@@ -12,8 +12,18 @@ public class Search {
 
     private String search_movie_name ;
     private String search_movie_imdb;
+    private String search_movie_id;
+
     String lalala;
     private ArrayList<Movies> searchresult_movies;
+    private ArrayList<Directors> searchresult_directors;
+    private ArrayList<Editors> searchresult_editos;
+    private ArrayList<Genre> searchresult_genres;
+    private Ratings searchresult_rating;
+    private Movies searchresult_movie;
+
+
+
     public String SearchMovieByName() {
         connetion.GetDriver();
         connetion.Connection();
@@ -26,6 +36,25 @@ public class Search {
         return "1";
     }
 
+
+    public String SearchMovieDetail() {
+        connetion.GetDriver();
+        connetion.Connection();
+        MoviesDAO newMovieDAO = new MoviesDAO();
+        newMovieDAO.setMovieDAO(connetion.connect);
+        setSearchresult_movie(newMovieDAO.SearchMovieByMovieID(getSearch_movie_id()));
+        setSearchresult_directors(newMovieDAO.SearchMovieDirectorsByMoiveID(getSearch_movie_id()));
+        setSearchresult_editos(newMovieDAO.SearchMovieEditorsByMoiveID(getSearch_movie_id()));
+        System.out.print(getSearch_movie_id());
+        System.out.print(getSearchresult_directors());
+        RatingDAO newRatingDAO = new RatingDAO();
+        newRatingDAO.setRatingDAO(connetion.connect);
+        setSearchresult_rating(newRatingDAO.SearchRatingByMovieID(getSearch_movie_id()));
+        GenreDAO newGenreDAO = new GenreDAO();
+        newGenreDAO.setGenreDAO(connetion.connect);
+        setSearchresult_genres(newGenreDAO.SearchGenreByMovieID(getSearch_movie_id()));
+        return "1";
+    }
 
     public Connect getConnet() {
         return connetion;
@@ -59,5 +88,59 @@ public class Search {
         this.search_movie_name = search_movie_name;
     }
 
+    public String getLalala() {
+        return lalala;
+    }
 
+    public void setLalala(String lalala) {
+        this.lalala = lalala;
+    }
+
+    public String getSearch_movie_id() {
+        return search_movie_id;
+    }
+
+    public void setSearch_movie_id(String search_movie_id) {
+        this.search_movie_id = search_movie_id;
+    }
+
+    public ArrayList<Directors> getSearchresult_directors() {
+        return searchresult_directors;
+    }
+
+    public void setSearchresult_directors(ArrayList<Directors> searchresult_directors) {
+        this.searchresult_directors = searchresult_directors;
+    }
+
+    public ArrayList<Editors> getSearchresult_editos() {
+        return searchresult_editos;
+    }
+
+    public void setSearchresult_editos(ArrayList<Editors> searchresult_editos) {
+        this.searchresult_editos = searchresult_editos;
+    }
+
+    public Ratings getSearchresult_rating() {
+        return searchresult_rating;
+    }
+
+    public void setSearchresult_rating(Ratings searchresult_rating) {
+        this.searchresult_rating = searchresult_rating;
+    }
+
+    public Movies getSearchresult_movie() {
+        return searchresult_movie;
+    }
+
+    public void setSearchresult_movie(Movies searchresult_movie) {
+        this.searchresult_movie = searchresult_movie;
+    }
+
+    public ArrayList<Genre> getSearchresult_genres() {
+        return searchresult_genres;
+    }
+
+    public void setSearchresult_genres(ArrayList<Genre> searchresult_genres) {
+        this.searchresult_genres = searchresult_genres;
+    }
 }
